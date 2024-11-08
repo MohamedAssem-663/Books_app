674a59fb8aaa15815d269961a9bec603a99673f7
MohamedAssem-663 <mohamedassem663@gmail.com>
2024-11-07 20:32:54 -0800

    Best Seller list view part 1

--- lib/features/home/prisintation/views/widgets/home_view_body.dart
+++ lib/features/home/prisintation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:books_app/core/Utils/assets.dart';
 import 'package:books_app/core/Utils/styles.dart';
 import 'package:books_app/features/home/prisintation/views/widgets/custom_app_bar.dart';
 import 'package:books_app/features/home/prisintation/views/widgets/featured_list_view.dart';
@@ -18,7 +19,46 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.titleMedium,
-          )
+          ),
+          BestSellerListView(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Column(
+            children: [
+              Text(
+                'Harry Potter and the Goblet of Fire ',
+              ),
+              Text('Harry Potter  ')
+            ],
+          ),
         ],
       ),
     );
