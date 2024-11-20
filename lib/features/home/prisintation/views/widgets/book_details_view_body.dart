f8718ee4947723505c8b8b0a72ad097ef581b58d
MohamedAssem-663 <mohamedassem663@gmail.com>
2024-11-19 21:29:20 -0800

    Add Titel &subtitle to book details view body

--- lib/features/home/prisintation/views/widgets/book_details_view_body.dart
+++ lib/features/home/prisintation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:books_app/core/Utils/styles.dart';
+import 'package:books_app/features/home/prisintation/views/widgets/book_rating.dart';
 import 'package:books_app/features/home/prisintation/views/widgets/custom_book_detials_app_bar.dart';
 import 'package:books_app/features/home/prisintation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -14,9 +16,30 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .15),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookImage(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const BookRating(),
         ],
       ),
     );
