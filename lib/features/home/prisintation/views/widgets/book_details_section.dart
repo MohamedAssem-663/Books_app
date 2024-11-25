ed0c445d11f07ce020f369b70aa210c06dbfc4f3
MohamedAssem-663 <mohamedassem663@gmail.com>
2024-11-25 12:54:39 -0800

    Refactor Book Details view body

---
+++ lib/features/home/prisintation/views/widgets/book_details_section.dart
@@ -0,0 +1,53 @@
+import 'package:books_app/core/Utils/styles.dart';
+import 'package:books_app/features/home/prisintation/views/widgets/book_rating.dart';
+import 'package:books_app/features/home/prisintation/views/widgets/books_action.dart';
+import 'package:books_app/features/home/prisintation/views/widgets/custom_book_item.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsSction extends StatelessWidget {
+  const BookDetailsSction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .2),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        Text(
+          'The Jungle Book',
+          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const BooksAction(),
+      ],
+    );
+  }
+}
