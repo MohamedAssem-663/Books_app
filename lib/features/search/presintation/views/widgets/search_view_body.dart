6132934d7f58b3729e89d627522cc3f195effd62
MohamedAssem-663 <mohamedassem663@gmail.com>
2024-11-26 14:57:04 -0800

    create search result list view widget

--- lib/features/search/presintation/views/widgets/search_view_body.dart
+++ lib/features/search/presintation/views/widgets/search_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:books_app/core/Utils/styles.dart';
-import 'package:books_app/features/home/prisintation/views/widgets/book_List_view_item.dart';
 import 'package:books_app/features/search/presintation/views/widgets/custom_search_text_field.dart';
+import 'package:books_app/features/search/presintation/views/widgets/search_result_List_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -27,25 +27,6 @@ class SearchViewBody extends StatelessWidget {
           Expanded(child: SearchResultListView()),
         ],
       ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: BookListViewItem(),
-        );
-      },
     );
   }
 }
