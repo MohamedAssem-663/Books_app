4de927eda24e23204958207ec514776e1d99d2a5
MohamedAssem-663 <mohamedassem663@gmail.com>
2024-12-23 21:15:23 -0800

    book details done

--- lib/features/home/presentation/views/widgets/books_action.dart
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -1,7 +1,7 @@
+import 'package:books_app/core/Utils/function/launch_url.dart';
 import 'package:books_app/core/widgets/custom_button.dart';
 import 'package:books_app/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({super.key, required this.bookModel});
@@ -26,17 +26,14 @@ class BooksAction extends StatelessWidget {
           ),
           Expanded(
             child: CustomButton(
-              onPressed: () async {
-                Uri uri = Uri.parse(bookModel.volumeInfo.previewLink!);
-                if (await canLaunchUrl(uri)) {
-                  !await launchUrl(uri);
-                }
+              onPressed: () {
+                launchCustomUrl(context, bookModel.volumeInfo.previewLink);
               },
               borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(14),
                 bottomRight: Radius.circular(14),
               ),
-              text: 'Preview',
+              text: getText(bookModel),
               textColor: Colors.white,
               backgroundColor: Colors.orange,
               fontSize: 16,
@@ -45,5 +42,13 @@ class BooksAction extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Avaliable';
+    } else {
+      return 'Preview';
+    }
   }
 }
